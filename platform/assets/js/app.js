9f6cb94a06be24a9f02d49f16797c7d283bc1ad2
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-01 09:30:44 +0300

    yarn encore production (Adding jQuery for testing). Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/assets/js/app.js
+++ platform/assets/js/app.js
@@ -7,6 +7,8 @@
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
