1ca7d34f30e78e1ea1d03a6f6b067523572d50d2
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-01 14:58:08 +0300

    Bumping the minimal required PHP version to 7.2.5. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
 // Valid PHP Version?
-$minPHPVersion = '7.2';
-if (phpversion() < $minPHPVersion)
+$minPHPVersion = '7.2.5';
+if (version_compare(PHP_VERSION, $minPHPVersion, '<'))
 {
-	die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . phpversion());
+	die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . PHP_VERSION);
 }
 unset($minPHPVersion);
 
