ec1ff3d71dd99b2f1792799a871d77feaa2917f4
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-02 02:46:33 +0300

    A correction. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/front/Views/welcome_message.php
+++ platform/applications/front/Views/welcome_message.php
@@ -50,14 +50,6 @@
 
     <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-    <p>If you would like to edit this page you will find it located at:</p>
-
-    <pre><code>app/Views/welcome_message.php</code></pre>
-
-    <p>The corresponding controller for this page can be found at:</p>
-
-    <pre><code>app/Controllers/Home.php</code></pre>
-
 </section>
 
 <div class="further">
