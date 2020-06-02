c9933b73ccfad5bbb119db01e42e34dc11a14731
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-02 06:23:37 +0300

    Reworking the bootstrapping process. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/bootstrap/bootstrap.php
+++ platform/bootstrap/bootstrap.php
@@ -13,12 +13,6 @@
  */
 require BOOTSTRAPPATH.'versions.php';
 
-if (version_compare(PHP_VERSION, PLATFORM_PHP_VERSION_MIN, '<')) {
-
-    header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-    echo 'PHP '.PLATFORM_PHP_VERSION_MIN.' or newer is required.';
-    exit(3);
-}
 
 /*
  * --------------------------------------------------------------------
@@ -201,6 +195,7 @@ define('PATHS_APP_DIRECTORY', realpath(APPPATH));
 define('PATHS_WRITABLE_DIRECTORY', realpath(WRITABLEPATH));
 define('PATHS_TESTS_DIRECTORY', realpath(TESTSPATH));
 define('PATHS_VIEW_DIRECTORY', realpath(VIEWPATH));
+
 
 /*
  * --------------------------------------------------------------------
--- platform/bootstrap/versions.php
+++ platform/bootstrap/versions.php
@@ -26,3 +26,11 @@ define('PRODUCT_VERSION', '1.0.0');
  * @var    string
  */
 define('PLATFORM_PHP_VERSION_MIN', '7.2.5');
+
+
+if (version_compare(PHP_VERSION, PLATFORM_PHP_VERSION_MIN, '<')) {
+
+    header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+    echo 'PHP '.PLATFORM_PHP_VERSION_MIN.' or newer is required.';
+    exit(3);
+}
