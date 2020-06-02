02c0411c958c86ce6117468d53f24b7917438359
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-02 09:17:17 +0300

    Reworking the bootstrapping process. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/bootstrap/bootstrap_ci.php
+++ platform/bootstrap/bootstrap_ci.php
@@ -31,7 +31,7 @@
  * @package    CodeIgniter
  * @author     CodeIgniter Dev Team
  * @copyright  2019-2020 CodeIgniter Foundation
- * @license    https://opensource.org/licenses/MIT	MIT License
+ * @license    https://opensource.org/licenses/MIT    MIT License
  * @link       https://codeigniter.com
  * @since      Version 4.0.0
  * @filesource
@@ -52,7 +52,7 @@
  */
 if (! defined('APPPATH'))
 {
-	define('APPPATH', realpath($paths->appDirectory) . DIRECTORY_SEPARATOR);
+    define('APPPATH', realpath($paths->appDirectory) . DIRECTORY_SEPARATOR);
 }
 
 /**
@@ -60,7 +60,7 @@ if (! defined('APPPATH'))
  */
 if (! defined('ROOTPATH'))
 {
-	define('ROOTPATH', realpath(APPPATH . '../') . DIRECTORY_SEPARATOR);
+    define('ROOTPATH', realpath(APPPATH . '../') . DIRECTORY_SEPARATOR);
 }
 
 /**
@@ -68,7 +68,7 @@ if (! defined('ROOTPATH'))
  */
 if (! defined('SYSTEMPATH'))
 {
-	define('SYSTEMPATH', realpath($paths->systemDirectory) . DIRECTORY_SEPARATOR);
+    define('SYSTEMPATH', realpath($paths->systemDirectory) . DIRECTORY_SEPARATOR);
 }
 
 /**
@@ -76,7 +76,7 @@ if (! defined('SYSTEMPATH'))
  */
 if (! defined('WRITEPATH'))
 {
-	define('WRITEPATH', realpath($paths->writableDirectory) . DIRECTORY_SEPARATOR);
+    define('WRITEPATH', realpath($paths->writableDirectory) . DIRECTORY_SEPARATOR);
 }
 
 /**
@@ -84,7 +84,7 @@ if (! defined('WRITEPATH'))
  */
 if (! defined('TESTPATH'))
 {
-	define('TESTPATH', realpath($paths->testsDirectory) . DIRECTORY_SEPARATOR);
+    define('TESTPATH', realpath($paths->testsDirectory) . DIRECTORY_SEPARATOR);
 }
 
 /*
@@ -92,15 +92,25 @@ if (! defined('TESTPATH'))
  * GRAB OUR CONSTANTS & COMMON
  * ---------------------------------------------------------------
  */
-if (! defined('APP_NAMESPACE'))
+if (file_exists(APPPATH . 'Config/Constants.php'))
 {
-	require_once APPPATH . 'Config/Constants.php';
+    require_once APPPATH . 'Config/Constants.php';
+}
+
+if (file_exists(COMMONPATH . 'Config/Constants.php'))
+{
+    require_once COMMONPATH . 'Config/Constants.php';
 }
 
 // Let's see if an app/Common.php file exists
 if (file_exists(APPPATH . 'Common.php'))
 {
-	require_once APPPATH . 'Common.php';
+    require_once APPPATH . 'Common.php';
+}
+
+if (file_exists(COMMONPATH . 'Common.php'))
+{
+    require_once COMMONPATH . 'Common.php';
 }
 
 // Require system/Common.php
@@ -118,8 +128,8 @@ require_once SYSTEMPATH . 'Common.php';
 
 if (! class_exists(Config\Autoload::class, false))
 {
-	require_once APPPATH . 'Config/Autoload.php';
-	require_once APPPATH . 'Config/Modules.php';
+    require_once APPPATH . 'Config/Autoload.php';
+    require_once APPPATH . 'Config/Modules.php';
 }
 
 require_once SYSTEMPATH . 'Autoloader/Autoloader.php';
@@ -129,7 +139,7 @@ require_once APPPATH . 'Config/Services.php';
 // Use Config\Services as CodeIgniter\Services
 if (! class_exists('CodeIgniter\Services', false))
 {
-	class_alias('Config\Services', 'CodeIgniter\Services');
+    class_alias('Config\Services', 'CodeIgniter\Services');
 }
 
 $loader = CodeIgniter\Services::autoloader();
@@ -139,17 +149,17 @@ $loader->register();    // Register the loader with the SPL autoloader stack.
 // Now load Composer's if it's available
 if (is_file(COMPOSER_PATH))
 {
-	/**
-	 * The path to the vendor directory.
-	 *
-	 * We do not want to enforce this, so set the constant if Composer was used.
-	 */
-	if (! defined('VENDORPATH'))
-	{
-		define('VENDORPATH', realpath(ROOTPATH . 'vendor') . DIRECTORY_SEPARATOR);
-	}
+    /**
+     * The path to the vendor directory.
+     *
+     * We do not want to enforce this, so set the constant if Composer was used.
+     */
+    if (! defined('VENDORPATH'))
+    {
+        define('VENDORPATH', realpath(ROOTPATH . 'vendor') . DIRECTORY_SEPARATOR);
+    }
 
-	require_once COMPOSER_PATH;
+    require_once COMPOSER_PATH;
 }
 
 // Load environment settings from .env files
