79cccc473b77f8256a084188405d5bc3a5d0f010
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-06 00:53:32 +0300

    More pretty path names. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/System/View/View.php
+++ platform/common/System/View/View.php
@@ -265,8 +265,15 @@ class View implements RendererInterface
 
             if (in_array(\CodeIgniter\Debug\Toolbar\Collectors\Views::class, $toolbarCollectors))
             {
+                $pretty_paths = ['APPPATH', 'COMMONPATH', 'SYSTEMPATH', 'ROOTPATH'];
+
+                if (defined('VENDORPATH'))
+                {
+                    $pretty_paths[] = 'VENDORPATH';
+                }
+
                 // Clean up our path names to make them a little cleaner
-                foreach (['APPPATH', 'SYSTEMPATH', 'ROOTPATH'] as $path)
+                foreach ($pretty_paths as $path)
                 {
                     if (strpos($this->renderVars['file'], constant($path)) === 0)
                     {
