14fe8dcb38cd3193d3d210e053c2b4c0f816d1bc
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-08 03:33:01 +0300

    Working on View drivers - a method for recognizing specific renderer/parser options. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

---
+++ platform/common/Modules/System/View/Driver.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Modules\System\View;
+
+class Driver
+{
+    public static function valid_drivers()
+    {
+        $result = [
+            'parser',
+            'twig',
+        ];
+
+        return $result;
+    }
+
+    protected static function parse_options($options)
+    {
+        if (!is_array($options)) {
+
+            $options = (string) $options;
+            $options = $options != '' ? [$options] : [];
+        }
+
+        if (empty($options)) {
+
+            return $options;
+        }
+
+        $drivers = [];
+        $result = [];
+
+        foreach ($options as $key => $value) {
+
+            if (is_string($key)) {
+
+                if (in_array($key, static::valid_drivers())) {
+
+                    $drivers[] = [['driver' => $key], ['options' => $value]];
+
+                } else {
+
+                    $result[$key] = $value;
+                }
+
+            } elseif (is_string($value)) {
+
+                if (in_array($value, static::valid_drivers())) {
+
+                    $drivers[] = [['driver' => $value], ['options' => []]];
+
+                } else {
+
+                    $result[] = $value;
+                }
+
+            }
+        }
+
+        if (!empty($drivers)) {
+
+            $options['drivers'] = $drivers;
+        }
+
+        return $options;
+    }
+
+}
