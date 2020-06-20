c7103ac64854c11b36eb88d58ba09f1d4626445f
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-20 18:02:51 +0300

    Adding the helper function view_string(). Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/Common.php
+++ platform/common/Common.php
@@ -13,3 +13,41 @@
  *
  * @link: https://codeigniter4.github.io/CodeIgniter4/
  */
+
+if (! function_exists('view_string'))
+{
+    /**
+     * Grabs the current RendererInterface-compatible class
+     * and tells it to render the specified view.
+     *
+     * NOTE: Does not provide any escaping of the data, so that must
+     * all be handled manually by the developer.
+     *
+     * @param string $stringTemplate
+     * @param array  $data
+     * @param array  $options - reserved for third-party extensions.
+     *
+     * @return string
+     */
+    function view_string(string $stringTemplate, array $data = [], array $options = []): string
+    {
+        /**
+         * @var CodeIgniter\View\View $renderer
+         */
+        $renderer = Services::renderer();
+
+        // The default option true might cause mess here.
+        //$saveData = config(View::class)->saveData;
+        $saveData = false;
+        //
+
+        if (array_key_exists('saveData', $options))
+        {
+            $saveData = (bool) $options['saveData'];
+            unset($options['saveData']);
+        }
+
+        return $renderer->setData($data, 'raw')
+                        ->renderString($stringTemplate, $options, $saveData);
+    }
+}
