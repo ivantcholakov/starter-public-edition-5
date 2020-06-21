9c21d06468af0441dde1e816fc7eab7f4e51a36b
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-22 02:31:48 +0300

    Uglifying some variable names. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/Modules/System/View/PHP.php
+++ platform/common/Modules/System/View/PHP.php
@@ -10,36 +10,36 @@ class PHP
     {
     }
 
-    public function render($____TEMPLATE____, array $____DATA____ = null, array $____OPTIONS____ = null)
+    public function render($____TeMpLaTe____, array $____DaTa____ = null, array $____OpTiOnS____ = null)
     {
-        unset($____OPTIONS____);
+        unset($____OpTiOnS____);
 
-        if (!empty($____DATA____)) {
-            extract($____DATA____);
+        if (!empty($____DaTa____)) {
+            extract($____DaTa____);
         }
 
-        unset($____DATA____);
+        unset($____DaTa____);
 
         ob_start();
-        include $____TEMPLATE____; // PHP will be processed
+        include $____TeMpLaTe____; // PHP will be processed
         $output = ob_get_contents();
         @ob_end_clean();
 
         return $output;
     }
 
-    public function renderString($____TEMPLATE____, array $____DATA____ = null, array $____OPTIONS____ = null)
+    public function renderString($____TeMpLaTe____, array $____DaTa____ = null, array $____OpTiOnS____ = null)
     {
-        unset($____OPTIONS____);
+        unset($____OpTiOnS____);
 
-        if (!empty($____DATA____)) {
-            extract($____DATA____);
+        if (!empty($____DaTa____)) {
+            extract($____DaTa____);
         }
 
-        unset($____DATA____);
+        unset($____DaTa____);
 
         ob_start();
-        $incoming = '?>' . $____TEMPLATE____;
+        $incoming = '?>' . $____TeMpLaTe____;
         eval($incoming);
         $output = ob_get_contents();
         @ob_end_clean();
