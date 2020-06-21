94bea7c8f8634d11670f81dcaacab231167dd77e
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-21 07:32:38 +0300

    Markdown: Corrections, no configuration is needed. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/Modules/Markdown/Markdown.php
+++ platform/common/Modules/Markdown/Markdown.php
@@ -12,18 +12,6 @@ class Markdown
 
     public function render($template, array $data = null, array $options = null)
     {
-        if (empty($data)) {
-            $data = [];
-        }
-
-        if (empty($options)) {
-            $options = [];
-        }
-
-        $config = config('Markdown')->config;
-        $options = array_merge_recursive_distinct($config, $options);
-        unset($config);
-
         $this->renderer = new \ParsedownExtra();
 
         return $this->renderer->text(file_get_contents($template));
@@ -31,18 +19,6 @@ class Markdown
 
     public function renderString($template, array $data = null, array $options = null)
     {
-        if (empty($data)) {
-            $data = [];
-        }
-
-        if (empty($options)) {
-            $options = [];
-        }
-
-        $config = config('Markdown')->config;
-        $options = array_merge_recursive_distinct($config, $options);
-        unset($config);
-
         $this->renderer = new \ParsedownExtra();
 
         return $this->renderer->text($template);
