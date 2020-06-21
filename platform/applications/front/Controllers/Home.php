af739a04bcc76194d277b92b2374e8ff3f5ac473
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-21 07:37:25 +0300

    Markdown: Testing the renderer. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/front/Controllers/Home.php
+++ platform/applications/front/Controllers/Home.php
@@ -4,15 +4,17 @@ class Home extends BaseController
 {
     public function index()
     {
-        $pass = 'jhdfks[eir]wm 9u57-m23v94u203wol vawjeklr';
-        $original_string = 'My Secret Message';
-        $encrypted_string = \GibberishAES::enc($original_string, $pass);
-        $decrypted_string = \GibberishAES::dec($encrypted_string, $pass);
+        $readme = null;
+        $readme_file = realpath(PLATFORMPATH.'../'.'README.md');
 
-        $twig = new \Common\Modules\Twig\Twig();
-        $hello = $twig->renderString('Hello {{ name }}!', ['name' => 'Ivan']);
+        if (is_file($readme_file)) {
 
-        return view('welcome_message.html', compact('original_string', 'encrypted_string', 'decrypted_string', 'hello'));
+            // This is a temporary, hard way to call the renderer.
+            $markdown = new \Common\Modules\Markdown\Markdown();
+            $readme = $markdown->renderString(file_get_contents($readme_file));
+        }
+
+        return view('welcome_message.html', compact('readme'));
     }
 
     //--------------------------------------------------------------------
