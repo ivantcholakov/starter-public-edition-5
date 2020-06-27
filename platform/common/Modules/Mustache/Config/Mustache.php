1614d31effc6c1ea634ac0f1075b6e69a87d9e77
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-27 12:25:53 +0300

    Mustache: More option checks. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/common/Modules/Mustache/Config/Mustache.php
+++ platform/common/Modules/Mustache/Config/Mustache.php
@@ -9,6 +9,38 @@ class Mustache extends BaseConfig
     public function __construct()
     {
         parent::__construct();
+
+        // Mustache Environment ----------------------------------------------
+
+        $this->config['template_class_prefix'] = '__Mustache_';
+
+        $this->config['cache'] = null;
+
+        $this->config['cache_file_mode'] = null;
+
+        $this->config['cache_lambda_templates'] = null;
+
+        $this->config['loader'] = null;
+
+        $this->config['partials_loader'] = null;
+
+        $this->config['partials'] = null;
+
+        $this->config['helpers'] = null;
+
+        $this->config['escape'] = null;
+
+        $this->config['entity_flags'] = null;
+
+        $this->config['charset'] = 'UTF-8';
+
+        $this->config['logger'] = null;
+
+        $this->config['strict_callables'] = null;
+
+        $this->config['delimiters'] = null;
+
+        $this->config['pragmas'] = null;
     }
 
     //------------------------------------------------------------------------
