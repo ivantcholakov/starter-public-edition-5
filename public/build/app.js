a709f554814e699fd8e148706e52490f793ec089
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-06-27 05:08:23 +0300

    Removing webpack related stuff. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- public/build/app.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["app"],{ng4s:function(n,o,s){"use strict";s.r(o);var e=s("EVdn"),i=s.n(e);s("sZ/o");window.jQuery=i.a,window.$=i.a,console.log("Hello Webpack Encore! Edit me in assets/js/app.js")},"sZ/o":function(n,o,s){}},[["ng4s","runtime",0]]]);
