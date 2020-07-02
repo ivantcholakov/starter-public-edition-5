e24722acfacfce5fe9fc37b52c6a00420587eaa2
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-07-02 03:20:32 +0300

    Scss renderer: Boilerplate code. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

---
+++ platform/common/Modules/Renderers/Config/Scss.php
@@ -0,0 +1,17 @@
+<?php namespace Common\Modules\Renderers\Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Scss extends BaseConfig
+{
+    public $config = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    //------------------------------------------------------------------------
+
+}
