51e5f09df7e245f4e397860e40931ce85c95e1bd
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-07-03 04:13:51 +0300

    Jsmin - testing rendering from a string. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/front/Controllers/Home.php
+++ platform/applications/front/Controllers/Home.php
@@ -12,7 +12,39 @@ class Home extends BaseController
             $readme = render_string(file_get_contents($readme_file), null, 'markdown');
         }
 
-        registry_set('test', render('test.js', null, 'jsmin'));
+        registry_set('test', render_string("
+/*
+ * test.js
+ */
+
+$(function() {
+
+    $('#sidebar_toggle').on('click', function(e) {
+
+        var body = $('body');
+        var state = '';
+
+        if (body.hasClass('sidebar-collapse')) {
+            state = 'sidebar-collapse';
+        }
+
+        $.ajax({
+            type: 'post',
+            mode: 'queue',
+            url: '/main-sidebar/toggle-ajax', // Adjust the URL here..
+            data: {
+                state: state
+            },
+            success: function(data) {
+
+            }
+        });
+    });
+
+});
+
+"
+        , null, 'jsmin'));
 
         return view('welcome_message', compact('readme'));
     }
