a45badeeed8ef63ab0e9e8bc51b61fc6f8a43274
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-07-03 08:00:13 +0300

    php spark assets:compile (initial implementation). Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

---
+++ platform/common/Commands/AssetsCompile.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Common\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+
+class AssetsCompile extends BaseCommand
+{
+    protected $group       = 'Assets';
+    protected $name        = 'assets:compile';
+    protected $description = 'Executes prepared tasks for web-assets compilation.';
+
+    protected $tasks;
+
+    public function __construct()
+    {
+        $this->tasks = config('AssetsCompile')->tasks;
+    }
+
+    public function run(array $params)
+    {
+        if (empty($this->tasks)) {
+            return;
+        }
+
+        $tasks = [];
+
+        if (empty($params)) {
+
+            // No specific name provided, process all the tasks.
+            // TODO: Revise the condition later.
+            $tasks = $this->tasks;
+
+        } else {
+
+            foreach ($params as $name) {
+
+                if ($key = $this->find($name) !== false) {
+                    $tasks[] = $this->tasks[$key];
+                }
+            }
+        }
+
+        if (empty($tasks)) {
+            return;
+        }
+
+        foreach ($tasks as $task) {
+
+            $source = isset($task['source']) ? (string) $task['source'] : '';
+
+            if (isset($task['source'])) {
+
+                if ($source == '') {
+                    continue;
+                }
+
+                if (!is_file($source)) {
+                    continue;
+                }
+
+                $task['source'] = $source;
+            }
+
+            $destination = isset($task['destination']) ? (string) $task['destination'] : '';
+
+            if (isset($task['destination'])) {
+
+                if ($destination == '') {
+                    continue;
+                }
+
+                $dir = pathinfo($destination, PATHINFO_DIRNAME);
+                file_exists($dir) OR mkdir($dir, DIR_WRITE_MODE, TRUE);
+
+                if (!is_dir($dir)) {
+                    continue;
+                }
+
+                $task['destination'] = $destination;
+            }
+
+            switch ($task['type']) {
+
+                case 'less':
+
+                    $this->less($task);
+
+                    break;
+
+                case 'scss':
+
+                    $this->scss($task);
+
+                    break;
+
+                case 'autoprefixer':
+
+                    $this->autoprefixer($task);
+
+                    break;
+
+                case 'cssmin':
+
+                    $this->cssmin($task);
+
+                    break;
+
+                case 'jsmin':
+
+                    $this->jsmin($task);
+
+                    break;
+            }
+
+            if (isset($task['destination'])) {
+
+                try {
+
+                    write_file($task['destination'], $task['result']);
+                    @chmod($task['destination'], FILE_WRITE_MODE);
+                    unset($task['result']);
+
+                    CLI::write(CLI::color($task['destination'], 'green'));
+
+                } catch(Exception $e) {
+
+                    echo CLI::write(CLI::color($e->getMessage(), 'yellow'));
+                }
+            }
+        }
+
+        CLI::write('');
+    }
+
+    protected function find($name)
+    {
+        $key = array_search($name, array_column($this->tasks, 'name'));
+
+        if (!is_int($key)) {
+            $key = false;
+        }
+
+        return $key;
+    }
+
+    protected function less(& $task)
+    {
+        $task['result'] = '';
+
+        $renderers = [];
+
+        $renderers['less'] = isset($task['less']) ? $task['less'] : [];
+
+        if (isset($task['autoprefixer'])) {
+            $renderers['autoprefixer'] = $task['autoprefixer'];
+        }
+
+        if (isset($task['cssmin'])) {
+            $renderers['cssmin'] = $task['cssmin'];
+        }
+
+        $task['result'] = render_string(file_get_contents($task['source']), null, $renderers);
+    }
+
+    protected function scss(& $task)
+    {
+        $task['result'] = '';
+
+        $renderers = [];
+
+        $renderers['scss'] = isset($task['scss']) ? $task['scss'] : [];
+
+        if (isset($task['autoprefixer'])) {
+            $renderers['autoprefixer'] = $task['autoprefixer'];
+        }
+
+        if (isset($task['cssmin'])) {
+            $renderers['cssmin'] = $task['cssmin'];
+        }
+
+        $task['result'] = render_string(file_get_contents($task['source']), null, $renderers);
+    }
+
+    protected function autoprefixer(& $task)
+    {
+        $task['result'] = '';
+
+        $renderers = [];
+
+        $renderers['autoprefixer'] = isset($task['autoprefixer']) ? $task['autoprefixer'] : [];
+
+        if (isset($task['cssmin'])) {
+            $renderers['cssmin'] = $task['cssmin'];
+        }
+
+        $task['result'] = render_string(file_get_contents($task['source']), null, $renderers);
+    }
+
+    protected function cssmin(& $task)
+    {
+        $task['result'] = '';
+
+        $renderers = [];
+
+        $renderers['cssmin'] = isset($task['cssmin']) ? $task['cssmin'] : [];
+
+        $task['result'] = render_string(file_get_contents($task['source']), null, $renderers);
+    }
+
+    protected function jsmin(& $task)
+    {
+        $task['result'] = '';
+
+        $renderers = [];
+
+        $renderers['jsmin'] = isset($task['jsmin']) ? $task['jsmin'] : [];
+
+        $task['result'] = render_string(file_get_contents($task['source']), null, $renderers);
+    }
+
+}
---
+++ platform/common/Config/AssetsCompile.php
@@ -0,0 +1,44 @@
+<?php namespace Common\Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class AssetsCompile extends BaseConfig
+{
+    public $tasks;
+
+    public function __construct() {
+
+        $this->tasks = [
+
+            // php spark assets:compile test-less-min
+            [
+                'name' => 'test-less-min',
+                'type' => 'less',
+                'source' => DEFAULTFCPATH.'assets/test-less.less',
+                'destination' => DEFAULTFCPATH.'assets/test-less.min.css',
+                'less' => ['relativeUrls' => false],
+                'autoprefixer' => ['browsers' => ['> 1%', 'last 2 versions', 'Firefox ESR', 'Safari >= 7', 'iOS >= 7', 'ie >= 10', 'Edge >= 12', 'Android >= 4']],
+                'cssmin' => [],
+            ],
+
+            // php spark assets:compile test-scss-min
+            [
+                'name' => 'test-scss-min',
+                'type' => 'scss',
+                'source' => DEFAULTFCPATH.'assets/test-scss.scss',
+                'destination' => DEFAULTFCPATH.'assets/test-scss.min.css',
+                'autoprefixer' => ['browsers' => ['> 1%', 'last 2 versions', 'Firefox ESR', 'Safari >= 7', 'iOS >= 7', 'ie >= 10', 'Edge >= 12', 'Android >= 4']],
+                'cssmin' => [],
+            ],
+
+            // php spark assets:compile test-jsmin
+            [
+                'name' => 'test-jsmin',
+                'type' => 'jsmin',
+                'source' => DEFAULTFCPATH.'assets/test-jsmin.js',
+                'destination' => DEFAULTFCPATH.'assets/test-jsmin.min.js',
+            ],
+
+        ];
+    }
+}
