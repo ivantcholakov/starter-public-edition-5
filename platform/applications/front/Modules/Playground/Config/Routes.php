e4863e50986bbe3ae2601df44ebddc02bf9f207c
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-07-21 07:16:06 +0300

    The Playground: Adding "Multiplayer Library Test", our first example. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/front/Modules/Playground/Config/Routes.php
+++ platform/applications/front/Modules/Playground/Config/Routes.php
@@ -1,5 +1,9 @@
 <?php
 
 $routes->group('playground', ['namespace' => 'Playground\Controllers'], function($routes) {
+
     $routes->get('/', 'Playground::index');
+
+    $routes->get('multiplayer', 'Multiplayer::index');
+
 });
---
+++ platform/applications/front/Modules/Playground/Controllers/Multiplayer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Playground\Controllers;
+
+class Multiplayer extends \Playground\Core\BaseController
+{
+    public function index()
+    {
+        $title = 'Multiplayer Library Test';
+
+        $this->breadcrumb->add($title, site_url('multiplayer'));
+        $this->header->setTitle($title);
+
+        $multiplayer = new \Common\Libraries\Multiplayer();
+
+        $videos = array(
+            'https://www.youtube.com/watch?v=NRhVcTTMlrM',
+            'http://vimeo.com/60743823',
+            'http://www.dailymotion.com/video/x9kdze',
+            'http://vbox7.com/play:25c4115f2d',
+        );
+
+        return view('Playground\Views\multiplayer', ['multiplayer' => $multiplayer, 'videos' => $videos]);
+    }
+
+}
