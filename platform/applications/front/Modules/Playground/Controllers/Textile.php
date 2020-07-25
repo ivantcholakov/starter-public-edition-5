0c68369015f1da28f991c070b1d1b9813726ee2c
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-07-25 21:51:49 +0300

    Using the function source($view). Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/front/Modules/Playground/Controllers/Textile.php
+++ platform/applications/front/Modules/Playground/Controllers/Textile.php
@@ -11,7 +11,7 @@ class Textile extends \Playground\Core\BaseController
         $this->breadcrumb->add($title, site_url('playground/textile'));
         $this->header->setTitle($title);
 
-        $text = str_replace('BASE_URL', base_url().'/', file_get_contents(locate('Playground\Views\test.textile')));
+        $text = str_replace('BASE_URL', base_url().'/', source('Playground\Views\test.textile'));
         $html = str_replace('BASE_URL', base_url().'/', render('Playground\Views\test.textile'));
 
         return view('Playground\Views\textile', ['text' => $text, 'html' => $html]);
