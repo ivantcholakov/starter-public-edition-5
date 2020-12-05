0a7c5c18e921b125ece956acbdded43511be4ec3
Ivan Tcholakov <ivantcholakov@gmail.com>
2020-12-05 09:44:31 +0200

    BaseController: Loading session driver. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/applications/front/Core/BaseController.php
+++ platform/applications/front/Core/BaseController.php
@@ -30,6 +30,11 @@ class BaseController extends Controller
     protected $helpers = [];
 
     /**
+     * @var type CodeIgniter\Session\Session
+     */
+    protected $session;
+
+    /**
      * @var \App\Libraries\MainMenu
      */
     public $mainMenu;
@@ -55,8 +60,8 @@ class BaseController extends Controller
         //--------------------------------------------------------------------
         // Preload any models, libraries, etc, here.
         //--------------------------------------------------------------------
-        // E.g.:
-        // $this->session = \Config\Services::session();
+
+        $this->session = \Config\Services::session();
 
         registry_set('controller', $this);
 
