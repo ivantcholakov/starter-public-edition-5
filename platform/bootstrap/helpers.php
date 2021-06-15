3cfe375d7398bf2961b66f3f65b9093c5e086f59
Ivan Tcholakov <ivantcholakov@gmail.com>
2021-06-15 23:51:44 +0300

    Adding the function preg_error_message().

--- platform/bootstrap/helpers.php
+++ platform/bootstrap/helpers.php
@@ -844,3 +844,74 @@ if (!function_exists('array_merge_recursive_distinct')) {
     }
 
 }
+
+// Miscelaneous ----------------------------------------------------------------
+
+/**
+ * @author Ivan Tcholakov, 2016-2021
+ * @license MIT
+ */
+
+if (!function_exists('preg_error_message')) {
+
+    function preg_error_message($code = null) {
+
+        static $preg_last_error_msg_exists = null;
+
+        if (!isset($preg_last_error_msg_exists)) {
+            $preg_last_error_msg_exists = function_exists('preg_last_error_msg');
+        }
+
+        if ($code === null) {
+
+            if ($preg_last_error_msg_exists) {
+
+                return preg_last_error_msg();
+            }
+
+            $code = preg_last_error();
+        }
+
+        $code = (int) $code;
+
+        switch ($code) {
+
+            case PREG_NO_ERROR:
+                $result = 'PCRE: No error, probably invalid regular expression.';
+                break;
+
+            case PREG_INTERNAL_ERROR:
+                $result = 'PCRE: Internal error.';
+                break;
+
+            case PREG_BACKTRACK_LIMIT_ERROR:
+                $result = 'PCRE: Backtrack limit has been exhausted.';
+                break;
+
+            case PREG_RECURSION_LIMIT_ERROR:
+                $result = 'PCRE: Recursion limit has been exhausted.';
+                break;
+
+            case PREG_BAD_UTF8_ERROR:
+                $result = 'PCRE: Malformed UTF-8 data.';
+                break;
+
+            default:
+
+                if (is_php('5.3') && $code == PREG_BAD_UTF8_OFFSET_ERROR) {
+                    $result = 'PCRE: Did not end at a valid UTF-8 codepoint.';
+                }
+                elseif (is_php('7') && $code == PREG_JIT_STACKLIMIT_ERROR) {
+                    $result = 'PCRE: Failed because of limited JIT stack space.';
+                }
+                else {
+                    $result = 'PCRE: Error '.$code.'.';
+                }
+
+                break;
+        }
+
+        return $result;
+    }
+
+}
